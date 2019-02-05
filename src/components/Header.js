77cb47a405aa1a0be1b05a4081a9f6644fd0e806
Carl Lang <carl.m.lang@gmail.com>
2019-02-05 18:36:01 +0000

    Added google fonts to typography plugin config

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
-import { Link } from "gatsby";
-import PropTypes from "prop-types";
 import React from "react";
+import PropTypes from "prop-types";
 import Nav from "./Atomic/Nav";
 
 const Header = ({ siteTitle, menuLinks }) => (
